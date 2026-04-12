a6b7883daeae3d3aa4d7457e7fcaa39128672fcd
mahmoud1amer2 <as0592489519@gmail.com>
2026-04-12 12:35:23 +0300

    Add movie routes to the server

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors())
+const routerMovies = require("./routes/movie-route")
+
+app.use(routerMovies)
 
 
 
