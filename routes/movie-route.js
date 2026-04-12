8040176f7acd21191beda3eee4ef6045dce1fe46
mahmoud1amer2 <as0592489519@gmail.com>
2026-04-12 13:14:09 +0300

    #4 get a single movie on json file

---
+++ routes/movie-route.js
@@ -0,0 +1,34 @@
+const express = require("express")
+
+const {getMovies,saveMovies} = require("../function/readAndWriteFunction")
+const router = express.Router()
+
+router.get('/movies',(req,res)=> {
+    try{
+        const dataMovies = getMovies()
+        return res.status(200).send(dataMovies)
+    }
+    catch(err) {
+        return res.status(500).send(err.message)
+    }
+})
+
+// ////////////////////////////GET/:ID//////////////////////////////////////////    
+
+router.get('/movies/:id',(req,res)=>{
+    try{  
+        const _id = req.params.id
+        const dataMovie = getMovies().find(movie => movie.id === parseInt(_id))
+        if(dataMovie){
+            return res.status(200).send(dataMovie)
+        }
+
+        res.status(404).send("the movie is not found")       
+    }
+    catch(err) {
+        res.status(500).send(err.message)
+    }
+})
+
+
+module.exports=router
