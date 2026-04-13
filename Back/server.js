c18c4f164c6ad1ffca3ebc8e6902c0a4b2d6b1d2
mahmoud1amer2 <as0592489519@gmail.com>
2026-04-13 14:05:21 +0300

    #8 setup-express-server

---
+++ Back/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(cors())
+const routerMovies = require("./routes/movie-route")
+
+app.use(routerMovies)
+
+app.get('/test',(req,res)=>{
+    res.send("the Server is working")
+})
+
+
+
+app.listen(port,()=> {
+   console.log(`The server is lisiting on port ${port}`)
+})
